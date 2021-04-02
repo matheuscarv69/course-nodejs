0cc3d4ff321faf336b41d4286820129ce0cb1f9b
matheuscarv69 <matheus9126@gmail.com>
2021-04-02 09:21:39 -0400

    :sparkles: feat: create newsController

---
+++ controllers/newsController.ts
@@ -0,0 +1,28 @@
+import NewsService from '../services/newsService';
+
+class NewsController {
+
+  get(req, res) {
+
+  }
+
+  getById(req, res) {
+
+  }
+
+  create(req, res) {
+
+  }
+
+  update(req, res) {
+
+  }
+
+  delete(req, res) {
+
+  }
+
+
+}
+
+export default new NewsController();
