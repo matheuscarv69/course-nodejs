7a1aa24a21d0207e8d5c6874a89bfcb3dd7f995a
matheuscarv69 <matheus9126@gmail.com>
2021-04-02 09:44:05 -0400

    :sparkles: feat: create methods get and getById

--- controllers/newsController.ts
+++ controllers/newsController.ts
@@ -1,13 +1,24 @@
 import NewsService from '../services/newsService';
+import * as HttpStatus from 'http-status';
 
 class NewsController {
 
-  get(req, res) {
+  sendResponse = function (res, statusCode, data) {
+    return res.status(statusCode).json({ result: data });
+  }
 
+  get(req, res) {
+    NewsService.get()
+      .then(news => this.sendResponse(res, HttpStatus.OK, news))
+      .catch(error => console.error.bind(console, `Error ${error}`))
   }
 
   getById(req, res) {
+    const _id = req.params.id;
 
+    NewsService.getById(_id)
+      .then(news => this.sendResponse(res, HttpStatus.OK, news))
+      .catch(error => console.error.bind(console, `Error ${error}`))
   }
 
   create(req, res) {
